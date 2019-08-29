9a32e8b9317198cd1fa6ede10070c1c7bb9ee3c6
Juan APblo <pin19087@uvg.edu.gt>
2019-08-29 11:42:51 -0600

    se está configurando el archivo para poder trabajarlo en equipo

--- Academico.java
+++ Academico.java
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
 public class Academico{
     private double notas;
     private String tareas[];
     private String tutorias[];
 
     
-=======
-public class Academico{
-    private double notas;
-    private String tareas[];
-    private String tutorias[];
-
-    
->>>>>>> 1f2f38e27e6ca9eaf836fb33d9c3b9b3335e3560
 }
+
+    
