14f30d9fb356f28e210d0030458092a160c977ee
Juan APblo <pin19087@uvg.edu.gt>
2019-08-29 11:43:34 -0600

    se está configurando el archivo para poder trabajarlo en equipo

--- Estudiante.java
+++ Estudiante.java
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
 public class Estudiante{
     private String nombre = "";
     private int edad = 0, alimentos = 0;
     private double peso = 0;
-    
-=======
-public class Estudiante{
-    
->>>>>>> 1f2f38e27e6ca9eaf836fb33d9c3b9b3335e3560
-}
+
+}    
