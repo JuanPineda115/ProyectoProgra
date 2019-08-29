1829da2440aa6f8acf87433fdbf6cdd6cb580476
Juan APblo <pin19087@uvg.edu.gt>
2019-08-29 11:33:26 -0600

    Se inicializaron las variables

---
+++ Eupepsia.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Eupepsia{
+public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    Academico ac = new Academico();
+    Estudiante es = new Estudiante();
+
+    
+
+    }
+}
