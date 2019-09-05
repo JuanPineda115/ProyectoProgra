8bc990966237c0f3335a00fe1733ff62c95e3365
pablo noack <noa17596@uvg.edu.gt>
2019-09-04 23:12:39 -0600

    \\En academico solo se cambiaron mayusculas, en  estudiante se definieron los metodos set,get para los atributos definidos. Se arreglaron los atributos

--- Estudiante.java
+++ Estudiante.java
@@ -1,6 +1,37 @@
 public class Estudiante{
-    private String nombre = "";
-    private int edad = 0, alimentos = 0;
-    private double peso = 0;
+    private String nombre ;
+    private int edad ; 
+	private int alimentos ;
+    private double peso ;
+	
+	public String getNombre(){
+		return nombre;
+	}
+	public void setNombre(String nombre){
+		this.nombre = nombre;
+	}
+	
+	public int getEdad(){
+		return edad;
+	}
+	
+	public void setEdad(int edad){
+		this.edad =  edad
+	}
+	 public int getAlimentos(){
+		 return alimentos;
+	 }
+	 
+	 public void setAlimentos(int alimentos){
+		 this.alimentos = alimentos;
+	 }
+	 
+	 public double getPeso(){
+		 return peso;
+	 }
+	 
+	 public void setPeso(double peso){
+		 this.peso = peso;
+	 }
 
 }    
