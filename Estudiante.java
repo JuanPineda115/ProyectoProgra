7fd981b8ae3928a279c19e431fa4ee4049535965
Martha30 <53914325+Martha30@users.noreply.github.com>
2019-09-05 19:35:46 -0600

    Update Estudiante.java

--- Estudiante.java
+++ Estudiante.java
@@ -3,12 +3,14 @@
 
 public class Estudiante{
 	// Atributos de la clase Estudiante //
+	//Se le pedirán más datos para tener acceso a cualquier inforamción sobre el estudiante//
     private String nombre ;
     private int edad ; 
 	private int alimentos ;
     private double peso ;
 	
 	// Metodos de la clase Estudiante // // set y get //
+	//Estos métodos se realizaron con el fin de obtener y mandar la inforamción del alumno//
 	public String getNombre(){
 		return nombre;
 	}
@@ -23,6 +25,7 @@ public class Estudiante{
 	public void setEdad(int edad){
 		this.edad =  edad;
 	}
+	//Esta variable en especifico, se modificara, ya que se necesita especificar que tipo de alimentos consume constantemente//
 	 public int getAlimentos(){
 		 return alimentos;
 	 }
